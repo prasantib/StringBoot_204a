4334dfd288cf3db43c0f0fddd0e3d4392c134e82
prasantib <prasanti.babu@gmail.com>
2019-10-08 15:43:47 -0400

    StringBoot_204a

---
+++ src/main/java/com/example/demo/StringBoot204aApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StringBoot204aApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StringBoot204aApplication.class, args);
+    }
+
+}
